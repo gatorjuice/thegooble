b27699006916b05ec25825b843b94ea8f9e64b2b
James Gates <gatorjuice@gmail.com>
2022-05-30 01:01:49 -0500

    deploy script

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheGooble />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheGooble from "@/components/TheGooble.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheGooble,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TheGooble.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>The Gooble</h1>
+  </div>
+  <div>
+    <input type="text" />
+  </div>
+  <div>
+    <button>The Gooble it!</button>
+  </div>
+</template>
+<script></script>
+<style></style>
