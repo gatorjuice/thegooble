a0ec20422bc6267a2890793adca5ec0a325631d0
James Gates <gatorjuice@gmail.com>
2022-05-31 09:10:23 -0500

    added axios

--- src/components/TheGooble.vue
+++ src/components/TheGooble.vue
@@ -2,12 +2,56 @@
   <div>
     <h1>The Gooble</h1>
   </div>
-  <div>
-    <input type="text" />
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--3-col"></div>
+    <div class="mdl-cell mdl-cell--6-col">
+      <form action="#" @submit.prevent="submitQuery">
+        <div class="mdl-textfield mdl-js-textfield">
+          <input
+            class="mdl-textfield__input"
+            type="text"
+            id="query"
+            v-model="query"
+          />
+          <label class="mdl-textfield__label" for="query">Search...</label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            @click="submitQuery"
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+          >
+            The Gooble it!
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="mdl-cell mdl-cell--3-col"></div>
   </div>
   <div>
-    <button>The Gooble it!</button>
+    <p>{{ results }}</p>
   </div>
 </template>
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      results: "",
+    };
+  },
+  mounted() {
+    this.results = "";
+  },
+  methods: {
+    submitQuery() {
+      if (this.query.toLowerCase() != "earthworms") {
+        this.results = "You spelled EARTHWORMS wrong";
+      } else {
+        this.results =
+          "Earthworms are a type of worm that are subdermal if the planet was skin.";
+      }
+    },
+  },
+};
+</script>
 <style></style>
